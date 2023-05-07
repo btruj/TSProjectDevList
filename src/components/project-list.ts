a5fb3b175dad5e5b541f6cf620fc206d9d3858b4
Bryan Trujillo <trujillobryan99@yahoo.com>
2023-05-07 12:56:10 -0400

    added webpack ready for deployment/production

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { DragTarget } from '../models/drag-drop.js';
-import { autobind } from '../decorators/autobind.js';
-import { projectState } from '../state/project-state.js';
-import  Component  from './base-component.js';
-import { Project, ProjectStatus } from '../models/project.js';
-import { ProjectItem } from './project-item.js';
+import { DragTarget } from '../models/drag-drop';
+import { autobind } from '../decorators/autobind';
+import { projectState } from '../state/project-state';
+import  Component  from './base-component';
+import { Project, ProjectStatus } from '../models/project';
+import { ProjectItem } from './project-item';
 
 //project list class
 export class ProjectList extends Component <HTMLDivElement, HTMLElement>
